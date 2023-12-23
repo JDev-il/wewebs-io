fd0e95d9f08bdc75d6d523a636fd52d340b3fb53
jdev-il <jonathandanielwork@gmail.com>
2023-12-23 18:31:14 +0200

    refined even more elements and breakpoints

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,6 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-container',
