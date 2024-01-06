259ab49039dcd42bf437fc3cb19f5e2f062c2ad9
jdev-il <jonathandanielwork@gmail.com>
2024-01-06 02:11:56 +0200

    improvements & responsive refinements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,18 +16,18 @@
 //   production: false
 // };
 
-// export const environment = {
-//   firebaseConfig: {
-//     apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
-//     authDomain: "jdev-il.firebaseapp.com",
-//     databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "jdev-il",
-//     storageBucket: "jdev-il.appspot.com",
-//     messagingSenderId: "704733248110",
-//     appId: "1:704733248110:web:6e1f6508ab6cfcafa8847f",
-//     measurementId: "G-SNHZQR0C3S"
-//   }
-// };
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
+    authDomain: "jdev-il.firebaseapp.com",
+    databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jdev-il",
+    storageBucket: "jdev-il.appspot.com",
+    messagingSenderId: "704733248110",
+    appId: "1:704733248110:web:6e1f6508ab6cfcafa8847f",
+    measurementId: "G-SNHZQR0C3S"
+  }
+};
 
 
 
