ef8332e0d5a4b5afeba9c7d6ef9418f53de29388
jdev-il <jonathandanielwork@gmail.com>
2024-01-06 02:46:23 +0200

    firebase configuration update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,28 +6,15 @@ export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
     authDomain: "jdev-il.firebaseapp.com",
-    databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app/",
+    databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "jdev-il",
     storageBucket: "jdev-il.appspot.com",
     messagingSenderId: "704733248110",
-    appId: "1:704733248110:web:0ae697f78ecb3c30a8847f",
-    measurementId: "G-WGS8XYJ146"
+    appId: "1:704733248110:web:6e1f6508ab6cfcafa8847f",
+    measurementId: "G-SNHZQR0C3S"
   },
   production: false
 };
-
-// export const environment = {
-//   firebaseConfig: {
-//     apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
-//     authDomain: "jdev-il.firebaseapp.com",
-//     databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "jdev-il",
-//     storageBucket: "jdev-il.appspot.com",
-//     messagingSenderId: "704733248110",
-//     appId: "1:704733248110:web:6e1f6508ab6cfcafa8847f",
-//     measurementId: "G-SNHZQR0C3S"
-//   }
-// };
 
 
 
