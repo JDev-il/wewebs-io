7d3c82c76faa4c9836e172071e78308155213675
jdev-il <jonathandanielwork@gmail.com>
2024-01-06 02:51:43 +0200

    --empty-commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,20 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// export const environment = {
+//   firebaseConfig: {
+//     apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
+//     authDomain: "jdev-il.firebaseapp.com",
+//     databaseURL: "https://jdev-il-default-rtdb.europe-west1.firebasedatabase.app/",
+//     projectId: "jdev-il",
+//     storageBucket: "jdev-il.appspot.com",
+//     messagingSenderId: "704733248110",
+//     appId: "1:704733248110:web:0ae697f78ecb3c30a8847f",
+//     measurementId: "G-WGS8XYJ146"
+//   },
+//   production: false
+// };
+
 export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyDzyBspL521SkJW1iU5zDyGdLEWAjLIzAk",
@@ -12,8 +26,7 @@ export const environment = {
     messagingSenderId: "704733248110",
     appId: "1:704733248110:web:6e1f6508ab6cfcafa8847f",
     measurementId: "G-SNHZQR0C3S"
-  },
-  production: false
+  }
 };
 
 
