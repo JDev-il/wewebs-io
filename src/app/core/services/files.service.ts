21b9ce40ba3c7ee4e459a2982dedd0e61f31cf69
jdev-il <jonathandanielwork@gmail.com>
2024-03-14 17:39:51 +0200

    regenrated url again..

--- src/app/core/services/files.service.ts
+++ src/app/core/services/files.service.ts
@@ -11,7 +11,7 @@ export class FilesService {
 
   constructor(@Inject(DOCUMENT) public document: Document) {
     const link: HTMLAnchorElement = this.document.createElement('a');
-    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2Fjonathan-daniel-cv.docx?alt=media&token=b7e765c5-2ba7-4048-82e7-0faf2880eb8c';
+    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2Fjonathan-daniel-cv.docx?alt=media&token=69a2f6db-aa41-4dbc-8695-541bf180d0d3';
     this.anchor = link;
   }
 
