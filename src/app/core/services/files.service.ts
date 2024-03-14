1d9b0ec98a828100c43f535a50a10e921288358f
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2024-03-14 12:39:13 +0200

    cv url changed

--- src/app/core/services/files.service.ts
+++ src/app/core/services/files.service.ts
@@ -11,7 +11,7 @@ export class FilesService {
 
   constructor(@Inject(DOCUMENT) public document: Document) {
     const link: HTMLAnchorElement = this.document.createElement('a');
-    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2Fjonathan-daniel-cv.docx?alt=media&token=6fafb570-bfb0-4303-b15a-7496621d9162';
+    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2Fjonathan-daniel-cv.docx?alt=media&token=1d4b3307-b501-440a-90b8-8d5ae458d4b9';
     this.anchor = link;
   }
 
