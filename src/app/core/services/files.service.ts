ab388319a04ebffd8e4ac499a1e88210754541fc
Jonathan Daniel <jonathandanielwork@gmail.com>
2024-06-19 20:41:11 +0300

    pdf cv change

--- src/app/core/services/files.service.ts
+++ src/app/core/services/files.service.ts
@@ -11,7 +11,7 @@ export class FilesService {
 
   constructor(@Inject(DOCUMENT) public document: Document) {
     const link: HTMLAnchorElement = this.document.createElement('a');
-    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2FResume%202024-Jonathan%20Daniel.pdf?alt=media&token=50d75842-a5de-41dd-8baa-918d584d2d5b';
+    link.href = 'https://firebasestorage.googleapis.com/v0/b/jdev-il.appspot.com/o/files%2FResume%202024-Jonathan%20Daniel.pdf?alt=media&token=9cecb7ab-b5a6-4c57-bf0c-6eb850216738';
     this.anchor = link;
   }
 
